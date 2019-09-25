0407365ecdb2363e84942021687bb0f3a5bc67fe
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-09-24 20:06:02 -0400

    The MOD library is no longer required.

--- fantastle-reboot/src/module-info.java
+++ fantastle-reboot/src/module-info.java
@@ -1,7 +1,6 @@
 module com.puttysoftware.fantastlereboot {
     requires java.desktop;
     requires com.puttysoftware.audio.wav;
-    requires com.puttysoftware.audio.mod;
     requires com.puttysoftware.desktop;
     requires com.puttysoftware.integration;
     requires com.puttysoftware.randomrange;
