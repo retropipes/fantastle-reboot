555e0f867c1355910b36ffa9afd3084103a54de9
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-09-29 18:15:02 -0400

    FantastleReboot now depends on Diane.

--- fantastle-reboot/src/module-info.java
+++ fantastle-reboot/src/module-info.java
@@ -1,7 +1,4 @@
 module com.puttysoftware.fantastlereboot {
     requires java.desktop;
-    requires com.puttysoftware.desktop;
-    requires com.puttysoftware.integration;
-    requires com.puttysoftware.randomrange;
-    requires com.puttysoftware.storage;
+    requires com.puttysoftware.diane;
 }
