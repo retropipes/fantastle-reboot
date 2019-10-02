09bbef71622fd631afffce3761ed2ccfc7b1d684
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-10-02 18:18:50 -0400

    Revert "Remove the image caches for all but the monster loader, which only loses its viewer."
    This reverts commit 8fe581c5c2aec8883979b8091135a4f4e482c487.

--- fantastle-reboot/src/com/puttysoftware/fantastlereboot/loaders/ItemImageLoader.java
+++ fantastle-reboot/src/com/puttysoftware/fantastlereboot/loaders/ItemImageLoader.java
@@ -18,17 +18,40 @@ Any questions should be directed to the author via email at: fantastle@worldwiza
  */
 package com.puttysoftware.fantastlereboot.loaders;
 
+import java.awt.FlowLayout;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Properties;
 
-import com.puttysoftware.diane.loaders.ImageLoader;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 import com.puttysoftware.fantastlereboot.FantastleReboot;
 import com.puttysoftware.fantastlereboot.assets.GameItemImage;
+import com.puttysoftware.fantastlereboot.utilities.ImageConstants;
+import com.puttysoftware.help.GraphicalHelpViewer;
 import com.puttysoftware.images.BufferedImageIcon;
 
 public class ItemImageLoader {
     private static String[] allFilenames;
     private static Properties fileExtensions;
+
+    static BufferedImageIcon loadUncached(final String name) {
+        try {
+            final URL url = ItemImageLoader.class
+                    .getResource("/assets/images/items/" + name);
+            final BufferedImage image = ImageIO.read(url);
+            final BufferedImageIcon icon = new BufferedImageIcon(image);
+            return icon;
+        } catch (final IOException ie) {
+            FantastleReboot.logError(ie);
+            return null;
+        }
+    }
 
     public static BufferedImageIcon load(GameItemImage image) {
         if (image != GameItemImage._NONE) {
@@ -45,10 +68,120 @@ public class ItemImageLoader {
                 }
             }
             String imageExt = fileExtensions.getProperty("images");
-            return ImageLoader.load(allFilenames[image.ordinal()] + imageExt,
-                    "/assets/images/items/", ItemImageLoader.class,
-                    FantastleReboot.getErrorHandler());
+            return ImageCache
+                    .getCachedImage(allFilenames[image.ordinal()] + imageExt);
         }
         return null;
+    }
+
+    public static void viewCache() {
+        if (!ImageCache.cacheCreated) {
+            ImageCache.createCache();
+        }
+        final GraphicalHelpViewer cv = new GraphicalHelpViewer(
+                ImageCache.images(), ImageCache.names());
+        final JFrame viewFrame = new JFrame("Item Image Cache Viewer");
+        viewFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        viewFrame.setLayout(new FlowLayout());
+        viewFrame.add(cv.getHelp());
+        cv.setHelpSize(ImageConstants.MAX_WINDOW_SIZE,
+                ImageConstants.MAX_WINDOW_SIZE);
+        viewFrame.pack();
+        viewFrame.setResizable(false);
+        viewFrame.setVisible(true);
+    }
+
+    public static void recreateCache() {
+        ImageCache.cacheCreated = false;
+        ImageCache.createCache();
+    }
+
+    private static class ImageCache {
+        // Fields
+        private static ArrayList<ImageCacheEntry> cache;
+        private static boolean cacheCreated = false;
+
+        // Methods
+        public static BufferedImageIcon getCachedImage(final String name) {
+            if (!ImageCache.cacheCreated) {
+                ImageCache.createCache();
+            }
+            for (ImageCacheEntry entry : ImageCache.cache) {
+                if (name.equals(entry.name())) {
+                    // Found
+                    return entry.image();
+                }
+            }
+            // Not found: Add to cache
+            BufferedImageIcon newImage = ItemImageLoader.loadUncached(name);
+            ImageCacheEntry newEntry = new ImageCacheEntry(newImage, name);
+            ImageCache.cache.add(newEntry);
+            return newImage;
+        }
+
+        public static BufferedImageIcon[] images() {
+            int limit = ImageCache.cache.size();
+            BufferedImageIcon[] result = new BufferedImageIcon[limit];
+            for (int x = 0; x < limit; x++) {
+                result[x] = ImageCache.cache.get(x).image();
+            }
+            return result;
+        }
+
+        public static String[] names() {
+            int limit = ImageCache.cache.size();
+            String[] result = new String[limit];
+            for (int x = 0; x < limit; x++) {
+                result[x] = ImageCache.cache.get(x).name();
+            }
+            return result;
+        }
+
+        private static void createCache() {
+            if (!ImageCache.cacheCreated) {
+                // Create the cache
+                ImageCache.cache = new ArrayList<>();
+                ImageCache.cacheCreated = true;
+            }
+        }
+    }
+
+    private static class ImageCacheEntry {
+        // Fields
+        private final BufferedImageIcon image;
+        private final String name;
+
+        // Constructors
+        public ImageCacheEntry(final BufferedImageIcon newImage,
+                final String newName) {
+            this.image = newImage;
+            this.name = newName;
+        }
+
+        // Methods
+        public BufferedImageIcon image() {
+            return this.image;
+        }
+
+        public String name() {
+            return this.name;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.name);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof ImageCacheEntry)) {
+                return false;
+            }
+            ImageCacheEntry other = (ImageCacheEntry) obj;
+            return Objects.equals(this.name, other.name);
+        }
     }
 }
