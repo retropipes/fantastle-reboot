8de8c36254d15d461016fb806a91491b6ad534af
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-10-20 09:32:33 -0400

    Remove duplicate code.

--- fantastle-reboot/src/com/puttysoftware/fantastlereboot/utilities/FormatConstants.java
+++
@@ -1,9 +0,0 @@
-package com.puttysoftware.fantastlereboot.utilities;
-
-public class FormatConstants {
-    public static final int MAZE_FORMAT_LATEST = 1;
-
-    private FormatConstants() {
-        super();
-    }
-}
