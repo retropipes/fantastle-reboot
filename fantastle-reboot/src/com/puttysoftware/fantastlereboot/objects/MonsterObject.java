2294e7a8ddc33df8b79da1e640c2c48408936a2c
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-10-27 19:31:55 -0400

    Make invisible monsters truly invisible.

--- fantastle-reboot/src/com/puttysoftware/fantastlereboot/objects/MonsterObject.java
+++ fantastle-reboot/src/com/puttysoftware/fantastlereboot/objects/MonsterObject.java
@@ -3,14 +3,24 @@ package com.puttysoftware.fantastlereboot.objects;
 import com.puttysoftware.fantastlereboot.assets.ObjectImageIndex;
 import com.puttysoftware.fantastlereboot.objectmodel.FantastleObject;
 import com.puttysoftware.fantastlereboot.objectmodel.MonsterObjectModel;
+import com.puttysoftware.images.BufferedImageIcon;
 
-public abstract class MonsterObject extends FantastleObject implements MonsterObjectModel {
+public abstract class MonsterObject extends FantastleObject
+    implements MonsterObjectModel {
+  private final boolean isVisible;
+
   protected MonsterObject(final boolean visible) {
     super(55, "monster", ObjectImageIndex.NORMAL_MONSTER);
+    this.isVisible = visible;
     this.setDestroyable(false);
-    if (!visible) {
-      this.setGameLook("invisible_monster", ObjectImageIndex.OPEN_SPACE);
-    }
     this.setSavedObject(new OpenSpace());
+  }
+
+  @Override
+  public BufferedImageIcon getGameImageHook() {
+    if (!this.isVisible) {
+      return this.getSavedObject().getImage();
+    }
+    return this.getGameImage();
   }
 }
