fefd1d3f71fc96f0f09ae05f1008cbecb50bbb9e
Eric Ahnell <eric.ahnell@puttysoftware.com>
2019-10-27 19:02:46 -0400

    Generate Ice in the dungeon 20% of the time.

--- fantastle-reboot/src/com/puttysoftware/fantastlereboot/objects/Ice.java
+++ fantastle-reboot/src/com/puttysoftware/fantastlereboot/objects/Ice.java
@@ -1,8 +1,10 @@
 package com.puttysoftware.fantastlereboot.objects;
 
 import com.puttysoftware.fantastlereboot.assets.ObjectImageIndex;
+import com.puttysoftware.fantastlereboot.maze.Maze;
 import com.puttysoftware.fantastlereboot.objectmodel.FantastleObject;
 import com.puttysoftware.fantastlereboot.objectmodel.Layers;
+import com.puttysoftware.randomrange.RandomRange;
 
 public final class Ice extends FantastleObject {
   public Ice() {
@@ -13,5 +15,16 @@ public final class Ice extends FantastleObject {
   @Override
   public int getLayer() {
     return Layers.GROUND;
+  }
+
+  @Override
+  public boolean shouldGenerateObject(Maze inMaze, int inRow, int inCol,
+      int inFloor, int inLevel, int inLayer) {
+    // Generate Ice 20% of the time
+    int dieRoll = RandomRange.generate(0, 4);
+    if (dieRoll != 0)
+      return false;
+    return super.shouldGenerateObject(inMaze, inRow, inCol, inFloor, inLevel,
+        inLayer);
   }
 }
