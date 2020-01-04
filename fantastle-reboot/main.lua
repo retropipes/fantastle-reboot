a80a2f71dd1cb1c5c8851dcdda6c45c13d6c1629
Eric Ahnell <eric.ahnell@puttysoftware.com>
2020-01-04 11:32:39 -0500

    Play music!

--- fantastle-reboot/main.lua
+++ fantastle-reboot/main.lua
@@ -1,14 +1,22 @@
 local push = require "push"
 
 function love.load()
+    -- Set up screen
     local gameWidth, gameHeight = 1080, 720
     local windowWidth, windowHeight = love.window.getDesktopDimensions()
     push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = true, resizable = true, highdpi = true, pixelperfect = true, canvas = false, stretched = false})
+    -- Cache the music for later
+    musics = {
+        title = love.audio.newSource("assets/music/title.ogg", "stream")
+    }
+    -- Set up joysticks
     local joysticks = love.joystick.getJoysticks()
     local joycount = love.joystick.getJoystickCount()
     if joycount >= 1 then
         joystick = joysticks[1]
     end
+    -- Play title music
+    love.audio.play(musics.title)
 end
 
 function love.draw()
@@ -26,10 +34,12 @@ end
 
 function love.update(dt)
     if love.keyboard.isScancodeDown("escape") then
+        love.audio.stop()
         love.event.quit(0)
     end
     if joystick then
         if joystick:isDown(9) then
+            love.audio.stop()
             love.event.quit(0)
         end
     end
