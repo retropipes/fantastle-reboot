cba7138b2c1563cdf7e09c1737ec4d9bf42c1f4b
Eric Ahnell <eric.ahnell@puttysoftware.com>
2020-01-04 12:59:08 -0500

    Add gamestate library and a loader state.

--- fantastle-reboot/main.lua
+++ fantastle-reboot/main.lua
@@ -1,33 +1,61 @@
+-- libraries
 local push = require "lib/push"
+local Gamestate = require "lib/hump/gamestate"
+
+-- assets
+local assets = {
+    images = {
+        avatars = {},
+        boss = {},
+        effects = {},
+        monsters = {},
+        objects = {},
+        ui = {}
+    },
+    music = {},
+    sounds = {}
+}
+
+-- game states
+local states = {
+    loader = {}
+}
 
 function love.load()
     -- Set up screen
     local gameWidth, gameHeight = 1080, 720
     local windowWidth, windowHeight = love.window.getDesktopDimensions()
     push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = true, resizable = true, highdpi = true, pixelperfect = false, canvas = false, stretched = true})
-    -- Cache UI assets
-    ui_images = {
-        logo = love.graphics.newImage("assets/images/ui/logo.png")
-    }
-    -- Cache the music for later
-    musics = {
-        title = love.audio.newSource("assets/music/title.ogg", "stream")
-    }
-    -- Set music options
-    musics.title:setLooping(true)
     -- Set up joysticks
     local joysticks = love.joystick.getJoysticks()
     local joycount = love.joystick.getJoystickCount()
     if joycount >= 1 then
         joystick = joysticks[1]
     end
-    -- Play title music
-    love.audio.play(musics.title)
+    -- Game state setup
+    Gamestate.registerEvents()
+    Gamestate.switch(states.loader)
 end
 
-function love.draw()
+function love.resize(w, h)
+    return push:resize(w, h)
+end
+
+-- Loader state
+function states.loader:init()
+    -- Cache logo image
+    assets.images.ui.logo = love.graphics.newImage("assets/images/ui/logo.png")
+    -- Cache title screen music
+    assets.music.title = love.audio.newSource("assets/music/title.ogg", "stream")
+    -- Set music options
+    assets.music.title:setLooping(true)
+    -- Play title music
+    love.audio.play(assets.music.title)
+end
+
+function states.loader:draw()
     push:start()
-    love.graphics.draw(ui_images.logo)
+    love.graphics.draw(assets.images.ui.logo)
     love.graphics.print({{0, 0, 0, 1}, "Press ESCAPE to quit"}, 400, 340)
     if joystick then
         love.graphics.print({{0, 0, 0, 1}, "Joystick detected; button 9 also quits"}, 400, 360)
@@ -35,11 +63,7 @@ function love.draw()
     push:finish()
 end
 
-function love.resize(w, h)
-    return push:resize(w, h)
-end
-
-function love.update(dt)
+function states.loader:update(dt)
     if love.keyboard.isScancodeDown("escape") then
         love.audio.stop()
         love.event.quit(0)
