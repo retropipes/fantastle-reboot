b8dd3ff644a04352214c890797c03f4889d0712a
Eric Ahnell <eric.ahnell@puttysoftware.com>
2020-01-04 10:25:32 -0500

    Run the game fullscreen, adapting to resolution.

--- fantastle-reboot/main.lua
+++ fantastle-reboot/main.lua
@@ -1,3 +1,20 @@
+local push = require "push"
+local gameWidth, gameHeight = 1080, 720
+local windowWidth, windowHeight = love.window.getDesktopDimensions()
+push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = true, resizable = false})
+
 function love.draw()
-    love.graphics.print("Fantastle Reboot in LÖVE goes here!", 400, 300)
+  push:start()
+  love.graphics.print("Press ESCAPE to quit", 400, 300)
+  push:finish()
+end
+
+function love.resize(w, h)
+    return push:resize(w, h)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+    if love.keyboard.isScancodeDown("escape") then
+        love.event.quit(0)
+    end
 end
