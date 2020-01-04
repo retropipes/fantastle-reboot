297fe91fc0eee75b91575650e98b6cbff9bab03b
Eric Ahnell <eric.ahnell@puttysoftware.com>
2020-01-04 11:27:55 -0500

    Add configuration. Change music extension to .ogg.

---
+++ fantastle-reboot/conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.identity = "com.puttysoftware.fantastle-reboot"
+    t.version = "11.3"
+    -- Disable unused modules
+    t.modules.physics = false
+end
--- fantastle-reboot/main.lua
+++ fantastle-reboot/main.lua
@@ -1,14 +1,12 @@
 local push = require "push"
-local gameWidth, gameHeight = 1080, 720
-local windowWidth, windowHeight = love.window.getDesktopDimensions()
-push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = true, resizable = false})
-hasjoystick = false
 
 function love.load()
+    local gameWidth, gameHeight = 1080, 720
+    local windowWidth, windowHeight = love.window.getDesktopDimensions()
+    push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {fullscreen = true, resizable = true, highdpi = true, pixelperfect = true, canvas = false, stretched = false})
     local joysticks = love.joystick.getJoysticks()
     local joycount = love.joystick.getJoystickCount()
     if joycount >= 1 then
-        hasjoystick = true
         joystick = joysticks[1]
     end
 end
@@ -16,8 +14,8 @@ end
 function love.draw()
     push:start()
     love.graphics.print("Press ESCAPE to quit", 400, 300)
-    if hasjoystick then
-        love.graphics.print("Joystick detected; button 9 also quits", 400, 400)
+    if joystick then
+        love.graphics.print("Joystick detected; button 9 also quits", 400, 350)
     end
     push:finish()
 end
